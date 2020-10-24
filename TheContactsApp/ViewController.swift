cd445bc98b08ac3b8ee8fa7deb84e0905869a758
John Kouris <jdkouris@gmail.com>
2020-10-24 11:06:50 -0700

    Initial Commit

---
+++ TheContactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheContactsApp
+//
+//  Created by John Kouris on 10/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
